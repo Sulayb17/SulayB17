0f06e3713bd7c64bbf23ccee3cefd86ede294494
Sulayman Batchilly <sbatchilly@sea.yearup.org>
2020-06-05 09:17:20 -0700

    New changes

--- pyBluezHack.py
+++ pyBluezHack.py
@@ -8,12 +8,12 @@ from bluetooth.ble import BeaconService #<---3rd party module
 
 #Create an instace of the object from the 3rd party class
 
-service = BeaconService()
+service = BeaconService() #Creating BeaconService as a variable into service
 
-service.start_advertising("11111111-2222-3333-4444-555555555555", 1, 1, 1, 200)
+service.start_advertising("11111111-2222-3333-4444-555555555555", 1, 1, 1, 200) #Parameters are how you recive the UUID
+                                       #UUID                       #Parameters
+time.sleep(15) #Sets amount of time the signal will last.
 
-time.sleep(15)
+service.stop_advertising()#This stops advertising the signal
 
-service.stop_advertising()
-
-print("Done.")
+print("Done.") #Send message letting us know its done
