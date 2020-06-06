f67e7728c16359faa31f98d0b90729471dfe24c1
Sulayman Batchilly <sbatchilly@sea.yearup.org>
2020-06-05 22:15:34 -0700

    With comments

--- selectionSort.py
+++ selectionSort.py
@@ -1,30 +1,30 @@
 
 #Make an array for the sorting of the selection
 
-array = [13, 6, 12, 6, 7, 2, 55]
+array = [13, 6, 12, 6, 7, 2, 55]#Create array of integers
 
-def selectionSort(array):
+def selectionSort(array):#Create function with array
 
-    n = len(array)
+    n = len(array)#Create Variables with array input
 
     for i in ranage(n): #Whatever the length ff the array is how many times you are going to run the for loop.
 
         #Initially assume the first element of the unsorted part as the minimum.
 
-        minimum = i
+        minimum = i#Set the minimum to if
 
-        for j in range(i+1, n):
+        for j in range(i+1, n):#For loop
 
             if (array[j] < array[minimum]): #Comparison operator
 
-            minimum = j
+            minimum = j#Event when conditions are met
 
         #Swap the minimum element with the first element of the unsorted part.
 
-        temp = array[i]
-        array[i] = array[minimum]
-        array[minimum] = temp
+        temp = array[i]#init  the array with i
+        array[i] = array[minimum]#initiating the array with the minimum
+        array[minimum] = temp#Initiates the minimum and thats what defines temp
 
-    return array
+    return array#Returns everything into the array
 
-print(selectionSort(array))
+print(selectionSort(array))#Prints the new array
